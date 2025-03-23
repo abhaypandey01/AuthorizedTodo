e4b3e7be4090d6ce7ebf7bd6e0e28cf4ef6e14c5
Abhay Pandey <abhaymp09@gmail.com>
2025-03-23 11:21:57 +0530

    Creating a authorized todo app

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='my-5 mx-3 py-4 px-5 text-white font-semibold bg-red-400 rounded-lg shadow-md'>App</div>
+  )
+}
+
+export default App
